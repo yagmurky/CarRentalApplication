ab2ef807371d4310b3d0b5fc7851cca0c099576b
yagmurky <yagmurky@users.noreply.github.com>
2024-03-06 14:58:32 +0300

    controller eklendi, auth'taki builder superbuilder olarak değiştirildi.

--- AuthService/src/main/java/com/yagmur/entity/Auth.java
+++ AuthService/src/main/java/com/yagmur/entity/Auth.java
@@ -5,9 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "tbl_auth")
