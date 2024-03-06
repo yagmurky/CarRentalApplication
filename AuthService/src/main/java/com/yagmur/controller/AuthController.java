f95b43aea46246d9924ed06cd9fd5ac56ae635e2
yagmurky <yagmurky@users.noreply.github.com>
2024-03-06 14:56:29 +0300

    controller eklendi

--- AuthService/src/main/java/com/yagmur/controller/AuthController.java
+++ AuthService/src/main/java/com/yagmur/controller/AuthController.java
@@ -1,4 +1,14 @@
 package com.yagmur.controller;
 
+import com.yagmur.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+    private final AuthService service;
+
 }
